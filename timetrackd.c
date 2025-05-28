6c8c5f119697c929e85ebb97fe76285a9c79b6ea
Sarad Gaihre <saradgaihre19@gmail.com>
2025-05-27 19:59:58 -0500

    wip: timetrack daemon and scafolding for inotify

--- timetrackd.c
+++ timetrackd.c
@@ -1,5 +1,7 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/inotify.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -8,7 +10,18 @@
 #define FIFO_PATH "/tmp/timetrack"
 #define FIFO_MODE 1337
 
-int listen(void) {
+struct Watchdog {
+  // The file descriptor associated with the current inotify instance
+  int inotify;
+
+  // flags with which the inotify instance is created
+  int flags;
+};
+
+// clean before exit
+void clean(struct Watchdog *wd) { free(wd); }
+
+int listen(int wd_fd) {
   char read_buf[BUF_SIZE];
 
   mkfifo(FIFO_PATH, FIFO_MODE);
@@ -27,8 +40,23 @@ int listen(void) {
 }
 
 // using inotify
-void watchdog(void) {
-  // TODO: implement
+int initialize_watchdog(void) {
+
+  // inotify
+  int inotify_fd = inotify_init();
+
+  if (inotify_fd == -1) {
+    perror("Failed to create a inotify instance");
+    return -1;
+  }
+
+  return inotify_fd;
+}
+
+// handle inotify events
+int inotify_loop(void) {
+  // todo
+  return 0;
 }
 
 int main(void) {
@@ -37,7 +65,13 @@ int main(void) {
   if (pid == -1) {
     perror("fork");
     return -1;
-  } else if (0 == pid)
-    listen();
+  } else if (0 == pid) {
+    int wd_fd = initialize_watchdog();
+
+    if (wd_fd == -1)
+      return -1;
+
+    listen(wd_fd);
+  }
   return 0;
 }
