bdb9b442b9b029d907d301f46d9cb984262e7a9f
Sarad Gaihre <saradgaihre19@gmail.com>
2025-07-21 18:34:37 -0500

    chore: add a more rigorious test

--- tests/hashmap.c
+++ tests/hashmap.c
@@ -13,25 +13,32 @@ void insert_get() {
   int k_3 = 10;
   char *v_3 = "When";
 
+  int k_4 = 65571;
+  char *v_4 = "Long";
+
   struct Table *map = hashmap();
 
   hashmap_insert(map, k_1, v_1);
   hashmap_insert(map, k_2, v_2);
   hashmap_insert(map, k_3, v_3);
+  hashmap_insert(map, k_4, v_4);
 
   char *res1 = hashmap_get(map, k_1);
   char *res2 = hashmap_get(map, k_2);
   char *res3 = hashmap_get(map, k_3);
+  char *res4 = hashmap_get(map, k_4);
 
   assert(strcmp(res1, "Hello") == 0);
   assert(strcmp(res2, "How") == 0);
   assert(strcmp(res3, "When") == 0);
+  assert(strcmp(res4, "Long") == 0);
 
   assert(strcmp(res2, "Hello") != 0);
 
   printf("%s\n", res1);
   printf("%s\n", res2);
   printf("%s\n", res3);
+  printf("%s\n", res4);
 
   return;
 }
